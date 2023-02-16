a40b065e6c1dcf19c8ac1cde460ec3b5a8e8ee21
Iskandar <iskandarsch3@mail.ru>
2023-02-16 18:31:28 +0500

    Первый коммит

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,98 @@
+package card
+
+import (
+	"bank/pkg/bank/types"
+)
+
+// Лимит снятия с карты 20 000сомони
+const withdrawLimit = 20_000_00
+
+// Withdraw функсия по снятию средств из карты
+func Withdraw(card types.Card, amount types.Money) types.Card {
+
+	// Проверяем если сумма меньше или равно 0 будет возврашена само карта
+	if amount <= 0 {
+		return card
+	}
+
+	// Проверяем если сумма больше лимита будет возврашена само карта
+	if amount > withdrawLimit {
+		return card
+	}
+
+	// Проверяем если карта не активна будет возврашена само карта
+	if !card.Active {
+		return card
+	}
+
+	// Проверяем если баланс карты меньше суммы будет возврашена само карта
+	if card.Balance < amount {
+		return card
+	}
+
+	// В этом случае из баланса карты будет снят сумма
+	card.Balance -= amount
+	return card
+}
+
+// Лимит зачисления на карту 50 000сомони
+const depositMoneyLimit = 50_000_00
+
+func Deposit(card *types.Card, amount types.Money) {
+
+	// Проверяем если карта не активна будет возврашена само карта
+	if !card.Active {
+		return
+	}
+
+	// Проверяем если сумма больше лимита будет возврашена само карта
+	if amount > depositMoneyLimit {
+		return
+	}
+
+	// В этом случае в баланс карты будет зачислен сумма
+	card.Balance += amount
+	return
+}
+
+func AddBonus(card *types.Card, percent int, daysInMonth int, daysInYear int) {
+
+	// Лимит зачисления на карту бонус 5 000сомони
+	const bonusLimit = 5_000_00
+
+	bonus := ((card.MinBalance * types.Money(percent)) / 100) * types.Money(daysInMonth) / types.Money(daysInYear)
+
+	// Проверяем если карта не активна будет возврашена само карта
+	if !card.Active {
+		return
+	}
+
+	// На карте сумма больше 0
+	if card.Balance <= 0 {
+		return
+	}
+
+	// Бонус не может быть выше 5 000сомони если выше вернет 5 000сомони
+	if bonus >= bonusLimit {
+		bonus = bonusLimit
+	}
+	// В этом случае в баланс карты будет зачислен бонус
+	card.Balance += bonus
+	return
+}
+
+func Total(cards []types.Card) types.Money {
+	summ := types.Money(0)
+	for _, card := range cards {
+		if !card.Active {
+			continue
+		}
+
+		if card.Balance <= 0 {
+			continue
+		}
+
+		summ += card.Balance
+	}
+	return summ
+}
---
+++ pkg/bank/card/example_test.go
@@ -0,0 +1,121 @@
+package card
+
+import (
+	"bank/pkg/bank/types"
+	"fmt"
+)
+
+// ExampleWithdraw_positive Успешное снятие баланса
+func ExampleWithdraw_positive() {
+	result := Withdraw(types.Card{Balance: 20_000_00, Active: true}, 10_000_00)
+	fmt.Println(result.Balance)
+	// Output: 1000000
+}
+
+// ExampleWithdraw_noMoney Попытка сниятия при нулевом балансе
+func ExampleWithdraw_noMoney() {
+	result := Withdraw(types.Card{Balance: 0, Active: true}, 10_000)
+	fmt.Println(result.Balance)
+	// Output: 0
+}
+
+// ExampleWithdraw_inactive Попытка сниятия баланса из неактивной карты
+func ExampleWithdraw_inactive() {
+	result := Withdraw(types.Card{Balance: 20_000_00, Active: false}, 10_000_00)
+	fmt.Println(result.Balance)
+	// Output: 3000000
+}
+
+// ExampleWithdraw_limit Попытка снятие баланса больше лимита(20 000сомони)
+func ExampleWithdraw_limit() {
+	result := Withdraw(types.Card{Balance: 20_000_00, Active: true}, 30_000_00)
+	fmt.Println(result.Balance)
+	// Output: 2000000
+}
+
+// ---------------//
+// ExampleDeposit_positive Успешное зачисление стредств
+func ExampleDeposit_positive() {
+	card := types.Card{Balance: 10_000_00, Active: true}
+	Deposit(&card, 5_000_00)
+	fmt.Println(card.Balance)
+	// Output: 1500000
+}
+
+// ExampleDeposit_inactive Зачисление стредств в неактивную карту
+func ExampleDeposit_inactive() {
+	card := types.Card{Balance: -10_000_00, Active: false}
+	Deposit(&card, 50_000_00)
+	fmt.Println(card.Balance)
+	// Output: 4000000
+}
+
+// ExampleDeposit_limit Успешное зачисление стредств до 50 000сомони
+func ExampleDeposit_limit() {
+	card := types.Card{Balance: 0, Active: true}
+	Deposit(&card, 50_000_00)
+	fmt.Println(card.Balance)
+	// Output: 5000000
+}
+
+// ExampleDeposit_active Успешное зачисление средств до 50 000сомони при минусовом балансе на карте
+func ExampleDeposit_active() {
+	card := types.Card{Balance: -10_000_00, Active: true}
+	Deposit(&card, 50_000_00)
+	fmt.Println(card.Balance)
+	// Output: 4000000
+}
+
+//--------------------//
+// ExampleAddBonus_positive Нормальное зачисление средств
+func ExampleAddBonus_positive() {
+	card := types.Card{Balance: 10_000_00, Active: true, MinBalance: 1000000}
+	AddBonus(&card, 3, 30, 365)
+	fmt.Println(card.Balance)
+	// Output: 1002465
+}
+
+// ExampleAddBonus_noactive Попытка зачислить на неактивную карту
+func ExampleAddBonus_noactive() {
+	card := types.Card{Balance: 10_000_00, Active: false, MinBalance: 10000}
+	AddBonus(&card, 3, 30, 365)
+	fmt.Println(card.Balance)
+	// Output: 1000024
+}
+
+// ExampleAddBonus_negativeBalance Попытка зачислить зачислить на карту с отрицательным балансом
+func ExampleAddBonus_negativeBalance() {
+	card := types.Card{Balance: -10_000_00, Active: true, MinBalance: 10000}
+	AddBonus(&card, 3, 30, 365)
+	fmt.Println(card.Balance)
+	// Output: 1000024
+}
+
+// ExampleAddBonus_moreBonus Попытка зачислить бонус более 5_000 сомони
+func ExampleAddBonus_moreBonus() {
+	card := types.Card{Balance: 1000000, Active: true, MinBalance: 100_000_000_00}
+	AddBonus(&card, 3, 30, 365)
+	fmt.Println(card.Balance)
+	// Output: 1500002
+}
+
+func ExampleTotal() {
+	cards := []types.Card{
+		{
+			Balance: 10_000_00,
+			Active:  true,
+		},
+		{
+			Balance: 10_000_00,
+			Active:  true,
+		},
+		{
+			Balance: 10_000_00,
+			Active:  true,
+		},
+	}
+
+	fmt.Println(Total(cards))
+	//Output: 3000000
+
+}
